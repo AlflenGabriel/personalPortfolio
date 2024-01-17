8bd21f2a393b34b1dea50e9ccfca1e60f7845767
AlflenGabriel <alflen.gabriel@gmail.com>
2024-01-17 12:23:17 -0300

    update /24

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,65 +1,82 @@
-import React from 'react'
-import './about.css'
-import Lottie from 'react-lottie';
-import animationData from './animation.json'
-import {FaAward} from 'react-icons/fa'
-import {CgWebsite} from 'react-icons/cg'
-import {ImMobile} from 'react-icons/im'
+import React from "react";
+import "./about.css";
+import Lottie from "react-lottie";
+import animationData from "./animation.json";
+import { FaAward } from "react-icons/fa";
+import { CgWebsite } from "react-icons/cg";
+import { ImMobile } from "react-icons/im";
 
 const defaultOptions = {
   loop: true,
-  autoplay: true, 
+  autoplay: true,
   animationData: animationData,
   rendererSettings: {
-  preserveAspectRatio: 'xMidYMid slice'
-  }
+    preserveAspectRatio: "xMidYMid slice",
+  },
 };
 
 const About = () => {
   return (
-    <section id='About'>
+    <section id="About">
       <h5>Get To Know</h5>
       <h2>About me</h2>
 
       <div className="container about__container">
         <div className="about__me">
-        <div className="about__me-animation">
-          <Lottie options={defaultOptions} />
+          <div className="about__me-animation">
+            <Lottie options={defaultOptions} />
           </div>
         </div>
 
         <div className="about__content">
           <div className="about__cards">
-            <article className='about__card'>
-              <FaAward className='about__icon'/>
+            <article className="about__card">
+              <FaAward className="about__icon" />
               <h5>Experience</h5>
-              <small>4+ Years Studing</small>
+              <small>2024 - Bachelor of Computer Science</small>
             </article>
 
-            <article className='about__card'>
-              <CgWebsite className='about__icon'/>
+            <article className="about__card">
+              <CgWebsite className="about__icon" />
               <h5>Front-end development</h5>
-              <small>Responsive websites built for an optimal user experience that achieves your business goals</small>
+              <small>
+                Responsive websites built for an optimal user experience that
+                achieves your business goals
+              </small>
             </article>
 
-            <article className='about__card'>
-              <ImMobile className='about__icon'/>
+            <article className="about__card">
+              <ImMobile className="about__icon" />
               <h5>Mobile Friendly</h5>
-              <small>A responsive design makes your website acessible to all users, regardless of their device</small>
+              <small>
+                A responsive design makes your website acessible to all users,
+                regardless of their device
+              </small>
             </article>
           </div>
           <p>
-          Front-End Developer with experience in create and maintain a better code base for reusability. Capable of continuous learning from senior developers facilitating projects from concept to launch. passionate about learning and development. Eager to tackle more complex problems and continue to find ways to maximize efficiency.
+            Front-End Developer with experience in create and maintain a better
+            code base for reusability. Capable of continuous learning from
+            senior developers facilitating projects from concept to launch.
+            passionate about learning and development. Eager to tackle more
+            complex problems and continue to find ways to maximize efficiency.
           </p>
           <p>
-          (PT-BR) Desenvolvedor Front-End com experiência em criar e manter uma base de código para reusabilidade. Capaz de aprender continuamente com senior developers, facilitando projetos desde o conceito até o lançamento. apaixonado por aprendizado e desenvolvimento. Ansioso para resolver problemas avançados e continuar a encontrar maneiras de maximizar a eficiência.
+            (PT-BR) Desenvolvedor Front-End com experiência em criar e manter
+            uma base de código para reusabilidade. Capaz de aprender
+            continuamente com senior developers, facilitando projetos desde o
+            conceito até o lançamento. apaixonado por aprendizado e
+            desenvolvimento. Ansioso para resolver problemas avançados e
+            continuar a encontrar maneiras de maximizar a eficiência.
           </p>
 
-          <a href="#Contact" className='btn btn-primary'>Let's Talk</a>
+          <a href="#Contact" className="btn btn-primary">
+            Let's Talk
+          </a>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import './experience.css'
-import {BsPatchCheckFill} from 'react-icons/bs'
+import React from "react";
+import "./experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
   return (
-    <section id='Experience'>
+    <section id="Experience">
       <h5>What Skills I Have</h5>
       <h2>My Experience</h2>
 
@@ -12,66 +12,75 @@ const Experience = () => {
         <div className="experience__frontend">
           <h3>Frontend Development</h3>
           <div className="experience__content">
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
-              <h4>HTML</h4>
-              <small className='text-light'>Experienced</small>
+                <h4>JavaScript</h4>
+                <small className="text-light">
+                  {" "}
+                  React - Next.js - jQuery - TypeScript - SEO
+                </small>
               </div>
             </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
-              <h4>CSS</h4>
-              <small className='text-light'>Experienced</small>
+                <h4>CSS</h4>
+                <small className="text-light">
+                  SASS - Tailwind - BootStrap
+                </small>
               </div>
             </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
-              <h4>bootstrap</h4>
-              <small className='text-light'>Basic</small>
+                <h4>Data Manipulation</h4>
+                <small className="text-light">
+                  {" "}
+                  Test - Cron - Docker - Deploy - Documentation
+                </small>
               </div>
             </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
-              <div><h4>REACT</h4>
-              <small className='text-light'>Intermediate</small></div>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>UX / UI</h4>
+                <small className="text-light">
+                  Design System - Sketch - Figma
+                </small>
+              </div>
             </article>
           </div>
         </div>
         <div className="experience__others">
-        <h3>Others</h3>
+          <h3>Others</h3>
           <div className="experience__content">
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
-              <h4>SCRUM Metodology</h4>
-              <small className='text-light'>Basic</small>
+                <h4>SCRUM Metodology</h4>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
-              <h4>SASS</h4>
-              <small className='text-light'>Intermediate</small>
+                <h4>BEM - Block Element Modifier</h4>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
-              <div><h4>BEM - Block Element Modifier</h4>
-              <small className='text-light'>Experienced</small></div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
-              <div><h4>MySQL</h4>
-              <small className='text-light'>Basic</small></div>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>MySQL</h4>
+                <small className="text-light">Experienced</small>
+              </div>
             </article>
           </div>
-          </div>
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
